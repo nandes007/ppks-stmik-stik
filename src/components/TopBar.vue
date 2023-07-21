a27b7057206bd4e92cde256aec547eaaef55e363
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2023-07-21 08:39:29 +0700

    initial commit

---
+++ src/components/TopBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="bg-[#343a40] py-4 px-5 flex justify-between">
+        <div class="flex items-center space-x-2">
+            <img src="../assets/images/location-icon.png" width="20" height="20">
+            <span class="text-white">Jl BRI Radio Dalam No 17 Jakarta Selatan</span>
+        </div>
+        <div class="flex items-center space-x-6 text-white">
+            <div class="flex space-x-2">
+                <img src="../assets/images/headset-icon.png" width="20" height="20">
+                <span>(021) 739 7973</span>
+            </div>
+            <div class="flex space-x-2">
+                <img src="../assets/images/email-icon.png" width="20" height="20">
+                <span>info@jak-stik.ac.id</span>
+            </div>
+        </div>
+    </div>
+</template>
