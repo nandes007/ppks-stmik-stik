4ceaff9445474f7ef8c3b7c27c94b86f87168fdf
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2023-07-21 08:58:07 +0700

    add menus in navbar

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,13 @@
 <script setup>
 import TopBar from './components/TopBar.vue'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <TopBar/>
+  <TopBar />
+  <NavBar />
+  
   <div>
-    <div class="fixed top-40 w-full">
-      <!-- Top Bar -->
-      <div class="top-bar hidden">
-        <div>
-          <div class="top-address">
-            Jl. BRI Radio Dalam
-          </div>
-          <div class="email-address">
-            Email
-          </div>
-        </div>
-        <div>
-          GRATIS
-        </div>
-      </div>
-      <!-- Top Bar End -->
-      <!-- Navbar -->
-      <div class="navbar flex justify-between items-center px-4 py-4 border-b-2 bg-white">
-        <div class="flex items-center space-x-2">
-          <div class="w-10 h-10">
-            <img src="./assets/images/logo.png">
-          </div>
-          <h1 class="font-semibold">Satgas PPKS STMIK STI&K Jakarta</h1>
-        </div>
-        <div>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-            <path fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
-          </svg>
-        </div>
-      </div>
-      <!-- Navbar Top -->
-    </div>
-
     <!-- Content -->
     <section class="px-4 pt-16">
       <div class="flex justify-center pt-8">
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- Navbar -->
+    <div class="navbar flex justify-between items-center px-4 py-4 border-b-2 bg-white">
+        <div class="flex items-center space-x-2">
+          <div class="w-10 h-10">
+            <img src="../assets/images/logo.png">
+          </div>
+          <h1 class="font-semibold">Satgas PPKS STMIK STI&K Jakarta</h1>
+        </div>
+        <div>
+            <ul class="flex space-x-6">
+                <li>Home</li>
+                <li>Perundangan & Kekerasan Seksual</li>
+                <li>Alur Pelaporan</li>
+                <li>FAQ</li>
+                <li>Tentang Kami</li>
+            </ul>
+        </div>
+        <div>
+            <a href="#" class="bg-[#ffc107] px-6 py-2 font-semibold font-serif rounded">Buat Aduan</a>
+        </div>
+        <!-- <div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+            <path fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+          </svg>
+        </div> -->
+      </div>
+      <!-- Navbar Top -->
+</template>
