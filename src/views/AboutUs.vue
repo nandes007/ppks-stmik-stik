4530ede728cf19c8279fef9704291b12c39303a2
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2024-02-06 21:28:57 +0700

    update: add sk satgas

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -2,50 +2,24 @@
 
 <template>
     <div class="pt-20 px-16">
-        <div class="flex justify-center pt-16 pb-32">
+        <div class="flex justify-center pt-16 pb-48">
             <div class="w-full">
-                <h1 class="text-4xl font-bold text-center">Tentang Kami</h1>
+                <h1 class="text-4xl font-bold text-center">SK SATGAS</h1>
                 <h5 class="text-center pt-4 text-slate-500 text-xl">Kami Siap Membantu Anda</h5>
 
                 <div class="pt-14 flex justify-center ">
                     <table class="table-fixed w-1/2">
                         <tr>
-                            <td class="border-b-2 border-slate-300 pr-16 pl-4 py-4">Pengarah</td>
-                            <td class="border-b-2 border-slate-300 pl-8">Rektor</td>
+                            <td class="border-b-2 border-slate-300 pr-16 pl-4 py-4">Ketua SATGAS</td>
+                            <td class="border-b-2 border-slate-300 pl-8">: Melani Dewi Lusita. S.Kom., MMSI</td>
                         </tr>
                         <tr>
-                            <td class="border-b-2 border-slate-300 pr-16 pl-4 py-4">Penanggung Jawab Teknis</td>
-                            <td class="border-b-2 border-slate-300 pl-8">#</td>
+                            <td class="border-b-2 border-slate-300 pr-16 pl-4 py-4">Sekretaris</td>
+                            <td class="border-b-2 border-slate-300 pl-8">: Laras Dewi Adisti. SP., MMSI</td>
                         </tr>
                         <tr>
-                            <td class="border-b-2 border-slate-300 pr-16 pl-4 py-4">Ketua</td>
-                            <td class="border-b-2 border-slate-300 pl-8">#</td>
-                        </tr>
-                        <tr>
-                            <td class="border-b-2 border-slate-300 pr-16 pl-4 py-4">Sekretaris I</td>
-                            <td class="border-b-2 border-slate-300 pl-8">#</td>
-                        </tr>
-                        <tr>
-                            <td class="border-b-2 border-slate-300 pr-16 pl-4 py-4">Sekretaris II</td>
-                            <td class="border-b-2 border-slate-300 pl-8">#</td>
-                        </tr>
-                        <tr>
-                            <td class="border-b-2 border-slate-300 pr-16 pl-4 py-4">Unsur Pendidik</td>
-                            <td class="border-b-2 border-slate-300 pl-8">
-                                <ul class="list-decimal leading-loose list-inside text-sm py-2">
-                                    <li>#</li>
-                                    <li>#</li>
-                                    <li>#</li>
-                                </ul>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="border-b-2 border-slate-300 pr-16 pl-4 py-4">Unsur Tenaga Kependidikan</td>
-                            <td class="border-b-2 border-slate-300 pl-8">#</td>
-                        </tr>
-                        <tr>
-                            <td class="border-b-2 border-slate-300 pr-16 pl-4 py-4">Unsur Mahasiswa</td>
-                            <td class="border-b-2 border-slate-300 pl-8">#</td>
+                            <td class="border-b-2 border-slate-300 pr-16 pl-4 py-4">Anggota</td>
+                            <td class="border-b-2 border-slate-300 pl-8">: Dr. Edi Pranoto. SE., MM., M.Ilkom</td>
                         </tr>
                     </table>
                 </div>
